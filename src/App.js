ecc0384ec90dcf593c7e007f82ca20c424a35a49
Cameron Miner <cameronsminer@gmail.com>
2022-11-29 18:22:22 -0700

    Added moviescreen file and mapped over/returned list of movie titles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from 'react'
-import Header from './components/Header'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Header from "./components/Header";
+import MovieScreen from "./components/MovieScreen";
+
+import "./App.css";
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`
+      )
+      .then((res) => {
+        console.log(res.data.results);
+        setMovieList(res.data.results);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [page]);
+
   return (
     <div className="App">
       <Header />
+      <main>
+        <MovieScreen movieList={movieList} page={page} setPage={setPage} watchlist={watchlist} />
+      </main>
     </div>
   );
 }
